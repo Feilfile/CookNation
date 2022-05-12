3f69cf17a402f009129cc807be266e4c68f3f38e
Matthias Feil <inf3491@hs-worms.de>
2022-05-12 12:41:36 +0200

    Firebase progress #1

---
+++ app/src/main/java/com/ema/cooknation/ActivityResultLauncher.kt
@@ -0,0 +1,25 @@
+package com.ema.cooknation
+
+class ActivityResultLauncher {
+/*
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    // Choose authentication providers
+    val providers = arrayListOf(
+        AuthUI.IdpConfig.EmailBuilder().build(),
+        AuthUI.IdpConfig.PhoneBuilder().build(),
+        AuthUI.IdpConfig.GoogleBuilder().build(),
+
+    // Create and launch sign-in intent
+    val signInIntent = AuthUI.getInstance()
+        .createSignInIntentBuilder()
+        .setAvailableProviders(providers)
+        .build()
+    signInLauncher.launch(signInIntent)
+
+ */
+}
