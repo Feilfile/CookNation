a2999faed0f9095f33b912ab3c82ec8af6363934
Matthias Feil <inf3491@hs-worms.de>
2022-05-16 15:09:58 +0200

    Firebase progress #3 Java -> Kotlin (string resource fix)

--- app/src/main/java/com/ema/cooknation/MainActivity.kt
+++ app/src/main/java/com/ema/cooknation/MainActivity.kt
@@ -3,14 +3,27 @@ package com.ema.cooknation
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private var mAuth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.m1_home)
-        mAuth = FirebaseAuth.getInstance()
+        setContentView(R.layout.main_activity)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainerView1)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.m1_home, R.id.m2_explore, R.id.m3_search, R.id.m4_profile))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
+
+       mAuth = FirebaseAuth.getInstance()
+
     }
 
     override fun onStart() {
--- app/src/main/java/com/ema/cooknation/ViewModel.kt
+++
@@ -1,5 +0,0 @@
-package com.ema.cooknation
-
-class ViewModel {
-
-}
