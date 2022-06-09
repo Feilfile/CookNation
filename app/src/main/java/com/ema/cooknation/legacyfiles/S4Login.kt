8b6843fabaf29001c278fa0833358cb95c230956
Matthias Feil <inf3491@hs-worms.de>
2022-06-09 22:26:12 +0200

    reorganizing files (deleting and creating)

---
+++ app/src/main/java/com/ema/cooknation/legacyfiles/S4Login.kt
@@ -0,0 +1,30 @@
+package com.ema.cooknation.legacyfiles/*package com.ema.cooknation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+
+class S4Login : Fragment() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_s4_login, container, false)
+    }
+
+}*/
